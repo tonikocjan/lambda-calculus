df2d90efb00f9339c2a095244370dd34d7e7efb4
Toni Kocjan <toni.kocjan@poviolabs.com>
2019-12-04 16:47:56 +0100

    Initial Commit

---
+++ λcalculus/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  λcalculus
+//
+//  Created by Toni Kocjan on 04/12/2019.
+//  Copyright © 2019 TSS. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
